49ec970fa4a959784b0e9a766fd5aade6181b7e5
Jonathan Correa Paiva <jona.correapc@gmail.com>
2022-07-27 19:51:07 -0300

    Update dbm.sh

--- dbm.sh
+++ dbm.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-java -jar /Database-Manager.jar -t `echo $DB_TYPE` -r `echo $DB_PATH` -u `echo $DB_USER` -p `echo $DB_PASSWORD`
+java -jar /Database-Manager.jar -t `echo $DB_TYPE` -r `echo $DB_URL` -u `echo $DB_USER` -p `echo $DB_PASSWORD`
